dfedaed54b5c21f563d5a8c9b5c8eea0a1129180
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-14 16:06:33 -0300

    feat: add simple data seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach(({ name, url}) => {
+    data.results.forEach(async ({ name, url}) => {
       const segments = url.split('/')
       const number:number = +segments[segments.length - 2]
 
-      console.log({name, number})
+      const pokemon = await this.pokemonModel.create( {name, number} )
     })
 
-    return data.results
+    return 'Seed excecuted.'
   }
 }
- 
