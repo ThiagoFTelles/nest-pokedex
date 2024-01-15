cf3afee56e74695b217656930b2a33a244990618
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-15 16:38:37 -0300

    feat: add nest configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
@@ -17,4 +20,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log( process.env )
+  }
+}
